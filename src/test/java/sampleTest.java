9d38752bb106fe722ba46a618fc71f51a37eeebe
Edwin <edwin.chavarro@neoris.com>
2024-06-25 15:11:01 -0500

    Se agrego dependencia para Lg4t

--- src/test/java/sampleTest.java
+++ src/test/java/sampleTest.java
@@ -1,11 +1,13 @@
 import com.intuit.karate.junit5.Karate;
 
-    class SampleTest {
+class SampleTest {
 
         @Karate.Test
         Karate testSample() {
             return Karate.run("users/sample.feature").relativeTo(getClass());
         }
+
     }
+
 
 
